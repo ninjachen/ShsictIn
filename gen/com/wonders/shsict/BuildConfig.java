8ba125934118f61ff286e45f6ff0025572e3e3af
jiachenning <jiachenning@gmail.com>
2014-01-27 15:20:13 +0800

    finish launch icon

---
+++ gen/com/wonders/shsict/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wonders.shsict;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/wonders/shsict/myWebview.java
@@ -0,0 +1,13 @@
+package com.wonders.shsict;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+public class myWebview extends WebView {
+
+	public myWebview(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
